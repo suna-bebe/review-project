e48fb29f3ea53192ac13d4eca729dbcc377f2fae
suna-bebe <suna7580mm@gmail.com>
2026-08-21 13:58:21 +0330

    I Create repository and project.py

---
+++ project.py
@@ -0,0 +1,16 @@
+# this is a easy code.
+num_1 = float(input(" enter first number: "))
+num_2 = float(input(" enter second number: "))
+
+operator = input(" select your oprerator ( *, /, +, _): ")
+
+if operator == "+":
+    print(num_1 + num_2)
+elif operator == "-":
+    print(num_1 - num_2)
+elif operator == "*":
+    print(num_1 * num_2)
+elif operator == "/":
+    print(num_1 / num_2)
+
+else:print("That is not valid operator")
